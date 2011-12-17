34b9ffaa7e17f4bede2c85c393c8058762788541
Yuji Tomita Macbook <yuchant@gmail.com>
2011-12-16 19:52:21 -0800

    template idea updated. models now addresses no longer have int type types

--- time_tracker/projects/admin.py
+++ time_tracker/projects/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.conf.urls.defaults import patterns
 from django.shortcuts import render
 
-from .models import Project, ProjectHours
+from .models import Address, Project, ProjectHours, Invoice
 
 
 class ProjectHoursInline(admin.TabularInline):
@@ -17,17 +17,43 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', )
     inlines = [ProjectHoursInline]
 
+    actions = [
+        'action_generate_invoice',
+    ]
+
+    def action_generate_invoice(self, request, queryset):
+        if not queryset.count() == 1:
+            self.message_user(request, "Must only select 1 project to invoice")
+            return
+        project = queryset[0]
+        hours = project.projecthours_set.filter(paid=False)
+        
+        if not hours:
+            self.message_user(request, "No hours to charge")
+
+
+        invoice = project.invoice_set.create()
+        invoice.hours = hours
+        invoice.save() # saves m2m
+
+
+class InvoiceAdmin(admin.ModelAdmin):
     def get_urls(self):
-        urls = super(ProjectAdmin, self).get_urls()
+        urls = super(InvoiceAdmin, self).get_urls()
         my_urls = patterns('',
-            (r'^(?P<pk>\d+)/render_invoice/$', self.render_invoice)
+            (r'^(?P<pk>\d+)/render/$', self.render_invoice)
         )
         return my_urls + urls
 
 
     def render_invoice(self, request, pk):
         ctx = {}
+        invoice = Invoice.objects.select_related('project', 'hours').get(pk=pk)
+        ctx['invoice'] = invoice
+        ctx['project'] = invoice.project
         return render(request, "projects/invoices/template1.html", ctx)
 
 
+admin.site.register(Address)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
--- time_tracker/projects/models.py
+++ time_tracker/projects/models.py
@@ -2,18 +2,75 @@ from django.db import models
 
 # Create your models here.
 
+
+class Address(models.Model):
+    type = models.CharField(max_length=128, default='')
+    project = models.ForeignKey('Project', blank=True, null=True)
+
+    name = models.CharField(max_length=128, blank=True)
+    company = models.CharField(max_length=128, blank=True)
+    street1 = models.CharField(max_length=128, blank=True)    
+    street2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=128, blank=True)    
+    province = models.CharField(max_length=128, blank=True)
+    postal_code = models.CharField(max_length=128, blank=True)
+    country = models.CharField(max_length=128, blank=True)
+    phone = models.CharField(max_length=128, blank=True)
+    email = models.EmailField(max_length=128, blank=True)
+
+    def __unicode__(self):
+        return '{self.type}: {self.name} [{self.company}]'.format(
+            self=self,)
+
+
 class Project(models.Model):
-	name = models.CharField(max_length=528)
-	hourly_rate = models.DecimalField(max_digits=12, decimal_places=2)
+    name = models.CharField(max_length=528)
+    hourly_rate = models.DecimalField(max_digits=12, decimal_places=2)
+    
+    default_ship_address = models.ForeignKey(Address, related_name="default_ship_projects")
+    company = models.ForeignKey(Address, related_name="related_projects")
 
 
 class ProjectHours(models.Model):
-	project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project)
 
-	start_time = models.DateTimeField()
-	end_time = models.DateTimeField(blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(blank=True, null=True)
 
-	comment = models.TextField(blank=True)
+    minutes = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    comment = models.TextField(blank=True)
+    paid = models.BooleanField(default=False, blank=True)
 
-	def __unicode__(self):
-		return '{self.start_time} - {self.end_time}'.format(self=self)
+    def __unicode__(self):
+        return '{self.start_time} - {self.end_time}'.format(self=self)
+
+    def save(self, *args, **kwargs):
+        if self.start_time and self.end_time:
+            self.minutes = (self.end_time - self.start_time).seconds / 60
+        super(ProjectHours, self).save(*args, **kwargs)
+
+
+class Invoice(models.Model):
+    project = models.ForeignKey(Project)
+    paid = models.BooleanField(default=False, blank=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=12, default=0)
+    hours = models.ManyToManyField(ProjectHours, blank=True, null=True)
+
+    comment = models.TextField(blank=True)
+
+    PAYMENT_TYPE_CHOICES = [
+        ('Cash', 'Cash'),
+        ('Check', 'Check'),
+        ('PayPal', 'PayPal'),
+        ('Credit Card', 'Credit Card'),
+        ('Bank Transfer', 'Bank Transfer'),
+    ]
+    payment_type = models.CharField(max_length=256, choices=PAYMENT_TYPE_CHOICES)
+
+    billing_address = models.ForeignKey(Address, blank=True, null=True, related_name="+")
+    shipping_address = models.ForeignKey(Address, blank=True, null=True, related_name="+")
+
+    def calculate_amount(self):
+        return self.hours.aggregate(s=models.Sum('minutes')).get('s', 0)
+
+
