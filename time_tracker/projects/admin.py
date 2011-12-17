d50368efcba3c1742f3f095a3ff70725a39a1724
Yuji Tomita Macbook <yuchant@gmail.com>
2011-12-16 18:04:01 -0800

    added template

--- time_tracker/projects/admin.py
+++ time_tracker/projects/admin.py
@@ -1,17 +1,33 @@
+from django import http
 from django.contrib import admin
+
+from django.conf.urls.defaults import patterns
+from django.shortcuts import render
 
 from .models import Project, ProjectHours
 
 
 class ProjectHoursInline(admin.TabularInline):
-	model = ProjectHours
-	extra = 1
+    model = ProjectHours
+    extra = 1
 
+# where do project documents go?
 
 class ProjectAdmin(admin.ModelAdmin):
-	list_display = ('name', )
+    list_display = ('name', )
+    inlines = [ProjectHoursInline]
 
-	inlines = [ProjectHoursInline]
+    def get_urls(self):
+        urls = super(ProjectAdmin, self).get_urls()
+        my_urls = patterns('',
+            (r'^(?P<pk>\d+)/render_invoice/$', self.render_invoice)
+        )
+        return my_urls + urls
+
+
+    def render_invoice(self, request, pk):
+        ctx = {}
+        return render(request, "projects/invoices/template1.html", ctx)
 
 
 admin.site.register(Project, ProjectAdmin)
